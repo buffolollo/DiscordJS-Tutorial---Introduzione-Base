9d37b63b3bacf92af8d97901faf83642e337625c
Joele Buffolo <elia.stef006@gmail.com>
2022-07-16 12:41:55 +0200

    Introduzione base #1

--- index.js
+++ index.js
@@ -8,7 +8,4 @@ client.on("ready", () => {
     console.log("Ciao! sono pronto!");
 })
 
-client.login("OTk3NjA2OTM4OTQwNTUxMjA4.GLIJ7W.0jiEqk9tyWKZb1bwYcotp7FOfdTY4cSixePbJE");
-
-
-//OTk3NjA2OTM4OTQwNTUxMjA4.GLIJ7W.0jiEqk9tyWKZb1bwYcotp7FOfdTY4cSixePbJE
+client.login("token");
