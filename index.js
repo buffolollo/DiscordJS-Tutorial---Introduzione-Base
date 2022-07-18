1b275ee7eca2239a6b45f08117c7bac926e19f15
Joele Buffolo <elia.stef006@gmail.com>
2022-07-18 11:12:56 +0200

    Evento message

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require("./config.json")
 const Discord = require("discord.js");
 const intents = new Discord.Intents(32767);
 const client = new Discord.Client({
@@ -8,4 +9,16 @@ client.on("ready", () => {
     console.log("Ciao! sono pronto!");
 })
 
-client.login("token");
+client.on("messageCreate", (message) => {
+    if(message.content == "!ciao"){
+        message.delete();
+    }
+    if(message.content == "!ciao"){
+        message.reply("ciao")
+    }
+    if(message.content == "!ciao"){
+        message.channel.send("ciao")
+    }
+})
+
+client.login(config.token);
